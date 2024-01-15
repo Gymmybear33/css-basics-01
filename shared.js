0cad01623cbdd9a3763c3393a6d53d5c28287efc
Jayson <jayson.caminante@gmail.com>
2024-01-15 08:31:47 -0500

    added functionality to the modal using js

--- shared.js
+++ shared.js
@@ -1,5 +1,20 @@
-var backdrop = document.querySelector('.backdrop')
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var selectModalButtons = document.querySelector('.modal__actions .modal__action--negative')
 
-// console.dir(backdrop)
+// console.dir(backdrop);
 
-backdrop.style.display = "block"
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    })
+};
+
+for (var i = 0; i < selectModalButtons.length; i++) {
+    selectModalButtons[i].addEventListener('click', function() {
+        modal.style.display = 'none';
+        backdrop.style.display = 'none';
+    })
+}
