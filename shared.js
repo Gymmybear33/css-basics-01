c0e74cd567c7a3b038bee0e5ed89e642582aa825
Jayson <jayson.caminante@gmail.com>
2024-01-16 14:41:25 -0500

    added the classList to the shared.js

--- shared.js
+++ shared.js
@@ -11,8 +11,11 @@ var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open'; This would override the complete list
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
@@ -24,18 +27,23 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
 // }
 
 function closeModal() {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 }
 
 backdrop.addEventListener('click', function() {
-    mobileNav.style.display = 'none';
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
     closeModal();
 });
 modalNoButton.addEventListener('click', closeModal)
 
 toggleButton.addEventListener('click', function() {
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 });
 
