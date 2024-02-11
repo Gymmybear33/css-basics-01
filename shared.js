6644e9b2d16f1bf21a0f931211a4ee69e1aa2200
Jayson <jayson.caminante@gmail.com>
2024-02-11 13:22:39 -0500

    added console.log to the shared js

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ var modalNoButton = document.querySelector('.modal__action--negative')
 var selectPlanButtons = document.querySelectorAll('.plan button');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector(".main-nav__item--cta")
 
 
 
@@ -64,3 +65,14 @@ toggleButton.addEventListener('click', function() {
     
 });
 
+ctaButton.addEventListener('animationstart', function(event) {
+    console.log("Animation started", event)
+})
+
+ctaButton.addEventListener('animationend', function(event) {
+    console.log("Animation ended", event)
+})
+
+ctaButton.addEventListener('animationiteration', function(event) {
+    console.log("Animation iteration", event)
+})
